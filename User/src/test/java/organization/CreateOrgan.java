8099b537faa7cf00e52e9d43bf06654845019cd6
code with srnagur <acchinagur@gmail.com>
2024-07-20 09:55:03 +0530

    push the current code

---
+++ User/src/test/java/organization/CreateOrgan.java
@@ -0,0 +1,11 @@
+package organization;
+
+public class CreateOrgan {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("hello organnanme");
+
+	}
+
+}
