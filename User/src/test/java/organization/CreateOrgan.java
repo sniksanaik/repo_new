b853280d5387c6e1d5cae539c8ac0e1f8ef13cdf
sniksanaik <sniksa10@gmail.com>
2024-07-26 11:33:25 +0530

    push

--- User/src/test/java/organization/CreateOrgan.java
+++ User/src/test/java/organization/CreateOrgan.java
@@ -5,6 +5,8 @@ public class CreateOrgan {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		System.out.println("hello organnanme");
+		
+		System.out.println("new add to send");
 
 	}
 
